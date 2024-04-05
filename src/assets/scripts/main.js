4ec65385990cf325d8fc3ef728d48b2fa9572420
David Garcia Ballester <davidgballester@uoc.edu>
2024-04-05 23:03:59 +0200

    codigo para generar QRs

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -10,7 +10,25 @@
  * Write any other JavaScript below
  */
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
+import QrCodeWithLogo from "qrcode-with-logos";
+import image from "../images/bandera.png";
+
+const generateQRCodeWithLogo = async () => {
+  console.log("Generating QR code with logo...");
+  const inputUrl = document.getElementById("qr-code-text").value;
+  console.log("Input URL: ", inputUrl);
+  new QrCodeWithLogo({
+    canvas: document.getElementById("qr-code"),
+    content: inputUrl,
+    download: false,
+    width: 300,
+    logo: {
+      src: image,
+    },
+  });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("generate-qr-code");
+  button.addEventListener("click", generateQRCodeWithLogo);
+});
